031b52ba73ae46ed66070678046d36f42492702d
Pallavi <pallavid0808@gmail.com>
2021-10-05 15:15:59 +0200

    added footer etc

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,38 @@
 import { useState } from 'react';
 import './App.css';
 
-import MapDisplay from './components/MapDisplay'
-import UserInput from './components/UserInput'
+import MapDisplay from './components/MapDisplay';
+import UserInput from './components/UserInput';
 import SideBar from './components/SideBar';
+import Footer from './components/Footer';
 
 function App() {
-  const [latLng, setLatLng] = useState({})
-  
-  const coOrdinates= (value) => {
-    setLatLng(value)
-    
-}
-console.log(latLng)
-  return (
-    <div className="relative">
-    
-      <MapDisplay coOrdinates={coOrdinates}/>
-      {/* <UserInput/> */}
-   {Object.entries(latLng).length !== 0 && <div className = "w-screen h-screen bg-black bg-opacity-10 absolute z-10 top-0"></div>}
-      
-      
-      {Object.entries(latLng).length !== 0 && <SideBar lat={latLng.lat} lng={latLng.lng} coOrdinates={coOrdinates}/>}
-    </div>
-  );
+   const [latLng, setLatLng] = useState({});
+
+   const coOrdinates = (value) => {
+      setLatLng(value);
+   };
+   console.log(latLng);
+   return (
+      <div>
+         <div className="relative">
+            <MapDisplay coOrdinates={coOrdinates} />
+            {/* <UserInput/> */}
+            {Object.entries(latLng).length !== 0 && (
+               <div className="w-screen h-screen bg-black bg-opacity-10 absolute z-10 top-0"></div>
+            )}
+
+            {Object.entries(latLng).length !== 0 && (
+               <SideBar
+                  lat={latLng.lat}
+                  lng={latLng.lng}
+                  coOrdinates={coOrdinates}
+               />
+            )}
+         </div>
+         <Footer />
+      </div>
+   );
 }
 
 export default App;
-
-
-
