d5a0b88fff47e0a6703d33fcddfe74cbc7683547
Sashmu <sasmithanagesh@gmail.com>
2021-10-05 03:26:59 +0200

    added sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import './App.css';
-import MapDisplay from './MapDisplay'
+
+import MapDisplay from './components/MapDisplay'
+import UserInput from './components/UserInput'
+import SideBar from './components/SideBar';
 
 function App() {
-  return (
-    <div>
+  const [latLng, setLatLng] = useState({})
+  
+  const coOrdinates= (value) => {
+    setLatLng(value)
     
-      <MapDisplay/>
-
+}
+console.log(latLng)
+  return (
+    <div className="relative">
+    
+      <MapDisplay coOrdinates={coOrdinates}/>
+      {/* <UserInput/> */}
+   {Object.entries(latLng).length !== 0 && <div className = "w-screen h-screen bg-black bg-opacity-10 absolute z-10 top-0"></div>}
+      
+      
+      {Object.entries(latLng).length !== 0 && <SideBar lat={latLng.lat} lng={latLng.lng} coOrdinates={coOrdinates}/>}
     </div>
   );
 }
